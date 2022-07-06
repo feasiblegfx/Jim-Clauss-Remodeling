7d47cf437a15bbc924641eacd5db687b79de63d4
feasible <98857201+feasiblegfx@users.noreply.github.com>
2022-07-06 19:02:05 -0400

    fixed Admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/img');
-  
+  eleventyConfig.addPassthroughCopy('./src/admin');
 
   return {
     dir: {
